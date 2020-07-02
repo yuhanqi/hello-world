e1a6dfe049e2facf8d3421da3f997e30aa24d6e3
yuhanqi <1790560630@qq.com>
2020-07-02 18:15:29 +0800

    0417

--- SuperPrime2.cpp
+++ SuperPrime2.cpp
@@ -1,19 +1,55 @@
-//作业：面向对象设计以下框架的代码细节，程序能编译运行得到正确结果 
 #include <iostream>
+class SuperPrime {
 class Prime {
   public:
+  	SuperPrime():number(0) {
+  	SuperPrime():number(0) {   
+  	  size = 0;
+  	}
+  	SuperPrime(int n):number(n) {
+  	  size = 0;
+  	  split();   
   	Prime():number(0) {
 	}
+  	~SuperPrime() {
+  	  for (int i = 0; i < size; ++i)  
+		delete N[i]; 
   	Prime(int n):number(n) {
 	}
+  	bool isSuperPrime() {
+  	  split();
+  	  int a = sum();
+	  int b = multi();
+	  int c = squareSum();
+	  if (isPrime(number) && isPrime(a) && isPrime(b) && isPrime(c))
+  	  SuperPrime a(sum());    
+	  SuperPrime b(multi());
+	  SuperPrime c(squareSum());
+	  if (isPrime() && a.isPrime() && b.isPrime() && c.isPrime())
+	    return true; 
 	~Prime() {
 	}
   	bool isPrime() { 
-  	  //2到number-1的因子 
+  	  if(number==1||number==0)
+		return false;
+	  for(int i=2;i<number;i++)
+	  {
+		if(number%i==0)
+			return false;
+	  }
+			//2到number-1的因子 
   	  return false;
 	}
   private:
   	const int number;
+  	int N[100], size;
+  	bool isPrime(int n) { 
+
+  	SuperPrime *N[100];
+	int size;
+  	bool isPrime() { 
+  	  //2到number-1的因子 
+  	  return false;
 }; 
 class PrimeSet {
   public:
@@ -35,6 +71,7 @@ class PrimeSet {
 	  index += 1;
 	  return true;
 	}
+	void split() {
 	bool isAllPrime() {
 	  for(int i = 0; i < index; i++)
 	    if (!N[i]->isPrime())
@@ -47,57 +84,80 @@ class PrimeSet {
 };
 class SuperPrime {
   public:
-  	SuperPrime():number(0), pset(3) {  //为什么必须有？ 
-  	}
+	SuperPrime():number(0), pset(3) {   
+ 	}
   	SuperPrime(int n):number(n), pset(3) {
-  	  split();  //它就是构造对象 
+  	  split();   
 	}
   	~SuperPrime() {
 	}
-  	bool isSuperPrime() {
-  	  //怎么使用pset？ 
-  	  Prime p(number);
-	  if (p.isPrime())
-	    return true; 
-  	  return false;
-	}
+	bool isSuperPrime() {
+	//怎么使用pset？ 
+	    Prime p(number);
+	  		if (p.isPrime()&&pset.isAllPrime())
+			  return true; 
+	    return false;
+		}
+
   private:
   	const int number;
-  	PrimeSet pset;
-	void split() {   //工厂方法设计模式 
-	  // number split into N
+    PrimeSet pset;
+  	int size_N,N[10];
+	void split() {   
 	  int temp = number;
 	  while(temp > 0) {
+	  for(int i=0;temp > 0;i++) {
 	  	int n = temp % 10;
 	  	temp /= 10;
-	  	pset.add(n);  //作业：单个数字为对象？还是和/积/平方和为对象？ 
+	  	N[size] = new SuperPrime(n);   
+	  	size += 1;
+	  	N[i]=n;}
+	  	pset.add(sum());
+	  	pset.add(multi());
+		pset.add(squareSum());   
 	  } 
 	}
+
 	int sum() {
 	  return 0;
+		int sum_number=0;
+		for(int i=0;i<size_N;i++)
+		{
+			sum_number+=N[i];
+		}
+	  return sum_number;
 	}
 	int multi() {
 	  return 0;
+		int multi_number=1;
+		int i;
+		for(i=0;i<size_N;i++)
+		{
+			multi_number*=N[i];
+		}
+	  return multi_number;
 	}
 	int squareSum() {
 	  return 0;
+		int square_num=0;
+		for(int i=0;i<size_N;i++)
+		{
+			square_num+=(N[i]*N[i]);
+		}
+	  return square_num;
 	}
 };
-class SuperPrimeSet {
+class Set {
   public:
-  	SuperPrimeSet(int from, int to) {
-  	  size = to - from;
-  	  for (int i = from; i < to; i++)
-  	    set[i-from] = new SuperPrime(i);
+  	Set(int from, int to) {
+  	  size = 0;
 	}
-  	~SuperPrimeSet() {
-  	  for(int i = 0; i < size; i++)
-  	    delete set[i];
+  	~Set() {
 	}
   	int count() {
   	  int count = 0;
   	  for (int i = 0; i < size; i++)
-  	    if(set[i]->isSuperPrime())
+  	    if(set[i].isSuperPrime())
   	      count += 1;
 	  return count; 
 	}
@@ -111,14 +171,24 @@ class SuperPrimeSet {
 	  return sum; 
 	}
   private:
-  	SuperPrime **set;
-  	int size, index;
+  	SuperPrime set[1000];
+  	int size;
 };
+ };
 int main() {
+  Set ss(100, 999);
+  std::cout << "How Many: " << ss.count() << std::endl;
+  std::cout << "Sum is " << ss.sum() << std::endl;
   SuperPrime sp(113);
   if (sp.isSuperPrime())
     std::cout << "113 is SuperPrime" << std::endl;
   else
     std::cout << "113 is NOT SuperPrime" << std::endl;
   return 0;
+    SuperPrime sp(113);
+    if (sp.isSuperPrime())
+        std::cout << "113 is SuperPrime" << std::endl;
+    else
+        std::cout << "113 is NOT SuperPrime" << std::endl;
+    return 0;
 }
